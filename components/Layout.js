63cc06725e57d2ece319c2b0b0d60a8a3b38c28c
Scott Wagner <scottywags16@gmail.com>
2020-03-05 22:26:09 -0500

    add slide layout with footer

---
+++ components/Layout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import bitmoji from "./bitmoji.png";
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "relative",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      {children}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+        }}
+      >
+        <img
+          src={bitmoji}
+          alt="Scott Bitmoji"
+          style={{ width: "100px" }}
+        />
+        <span style={{ fontSize: "28px" }}>@nbbdog11</span>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
